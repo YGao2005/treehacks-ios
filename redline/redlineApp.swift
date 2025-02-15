f6032315c4ebfd9b7d90770ea7e0f1f631f0e9e3
YGao2005 <41841169+YGao2005@users.noreply.github.com>
2025-02-14 22:06:36 -0800

    Initial Commit

---
+++ redline/redlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  redlineApp.swift
+//  redline
+//
+//  Created by Yang Gao on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct redlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
